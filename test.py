ce46d26a8d5dd905d58bbb50f4691b89a98fa611
TacoTruckMan777 <jcruz91011@gmail.com>
2025-09-10 09:20:15 -0400

    stuff

--- test.py
+++ test.py
@@ -2,6 +2,16 @@ import turtle
 from turtle import *
 t = Turtle()
 t.shape('turtle')
-for i in range(4):
-    t.forward(100)
+def square(x):
+    t.forward(x)
     t.left(90)
+    t.forward(x)
+    t.left(90)
+    t.forward(x)
+    t.left(90)
+    t.forward(x)
+    t.left(90)
+t.speed(99999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999)
+for i in range(6009999999999999):
+    square(200)
+    t.right(0.1)
