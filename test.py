7baae4b104c9620b76f501457b670c75b80317e1
TacoTruckMan777 <jcruz91011@gmail.com>
2025-09-12 06:20:15 -0700

    Update test.py

--- test.py
+++ test.py
@@ -4,7 +4,7 @@ t = Turtle()
 t.shape('turtle')
 t.speed(9999)
 sidelength = 100
-rotate = 90
+rotate = 144
 def square(x,y):
    for i in range(4):
     t.forward(x)
@@ -12,7 +12,7 @@ def square(x,y):
 def AddSquares(iRange):
     length = 5
     for i in range(iRange):
-        square(length, 90)
+        square(length, 144)
         length += 5
         t.right(5)
 AddSquares(400)
