f37f6751e6c63449604cea614c50a0b30d193c0b
TacoTruckMan777 <jcruz91011@gmail.com>
2025-09-15 06:25:22 -0700

    Update test.py

--- test.py
+++ test.py
@@ -6,7 +6,7 @@ t.speed(9999)
 sidelength = 100
 rotate = 144
 def square(x,y):
-   for i in range(4):
+   for i in range(5):
     t.forward(x)
     t.left(y)
 def AddSquares(iRange):
@@ -15,4 +15,4 @@ def AddSquares(iRange):
         square(length, 144)
         length += 5
         t.right(5)
-AddSquares(400)
+AddSquares(600)
